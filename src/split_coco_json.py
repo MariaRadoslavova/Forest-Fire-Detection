8ae06cad5fe39271ee88e0e59e585fbf4bcbb231
MariaRadoslavova <rmarooa@gmail.com>
2024-08-31 09:53:34 -0700

    Create split_coco_json.py

---
+++ src/split_coco_json.py
@@ -0,0 +1,35 @@
+import json
+import random
+
+def split_coco_json(coco_json_path, train_json_path, test_json_path, train_ratio=0.8, seed=42):
+    random.seed(seed)
+
+    with open(coco_json_path, 'r') as f:
+        coco = json.load(f)
+
+    images = coco['images']
+    annotations = coco['annotations']
+    categories = coco['categories']
+
+    random.shuffle(images)
+    num_train = int(len(images) * train_ratio)
+    train_images = images[:num_train]
+    test_images = images[num_train:]
+
+    train_image_ids = set(img['id'] for img in train_images)
+    test_image_ids = set(img['id'] for img in test_images)
+
+    train_annotations = [ann for ann in annotations if ann['image_id'] in train_image_ids]
+    test_annotations = [ann for ann in annotations if ann['image_id'] in test_image_ids]
+
+    train_coco = {'images': train_images, 'annotations': train_annotations, 'categories': categories}
+    test_coco = {'images': test_images, 'annotations': test_annotations, 'categories': categories}
+
+    with open(train_json_path, 'w') as f:
+        json.dump(train_coco, f, indent=4)
+
+    with open(test_json_path, 'w') as f:
+        json.dump(test_coco, f, indent=4)
+
+    print(f"Training set: {len(train_images)} images, {len(train_annotations)} annotations")
+    print(f"Testing set: {len(test_images)} images, {len(test_annotations)} annotations")
