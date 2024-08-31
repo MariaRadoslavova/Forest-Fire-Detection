a2ffeada2d36688c468c4efdba1d1000a2790693
MariaRadoslavova <rmarooa@gmail.com>
2024-08-31 09:49:38 -0700

    Create analysis.py

---
+++ src/analysis.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+from collections import Counter
+import numpy as np
+import pandas as pd
+
+def plot_class_distribution(annotations):
+    class_counts = Counter(ann['category_id'] for ann in annotations)
+    plt.bar(class_counts.keys(), class_counts.values())
+    plt.xlabel('Category ID')
+    plt.ylabel('Number of Annotations')
+    plt.title('Class Distribution')
+    plt.show()
+
+def generate_statistics_report(annotations):
+    df = pd.DataFrame(annotations)
+    df['width'] = df['bbox'].apply(lambda bbox: bbox[2])
+    df['height'] = df['bbox'].apply(lambda bbox: bbox[3])
+    df['aspect_ratio'] = df['width'] / df['height']
+    print(df[['width', 'height', 'aspect_ratio']].describe())
+    df[['width', 'height']].plot(kind='hist', bins=30, alpha=0.5)
+    plt.title('Bounding Box Size Distribution')
+    plt.show()
+
+# More functions as needed...
